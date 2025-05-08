862cc92d0c2479cf7a75c29faba8694d681602a5
Jason Henderson <jason.a.henderson@gmail.com>
2025-05-08 17:30:21 -0400

    cleaned up post data, added series and author data, vercel config

--- astro.config.ts
+++ astro.config.ts
@@ -5,17 +5,19 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
 
-import netlify from "@astrojs/netlify";
-
+import vercel from '@astrojs/vercel';
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
 
-  integrations: [sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), decapCmsOauth()],
+  integrations: [
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    decapCmsOauth(),
+  ],
 
   // https://astro.build/en/reference/configuration-reference/#output
   // Use static, and in places where routing is needed for dynamic content, use the <a href="https://astro.build/en/core-concepts/routing/#dynamic-routes">dynamic routes</a> feature.
@@ -52,5 +54,9 @@ export default defineConfig({
     preserveScriptOrder: true,
   },
 
-  adapter: netlify(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
---
+++ src/data/authors/jason-henderson.md
@@ -0,0 +1,9 @@
+---
+title: J. A. Henderson
+name:
+  firstName: Jason
+  lastName: Henderson
+id: "1"
+ogImage: 015-wyclx2vwpm8.jpeg
+slug: "jason-henderson"
+---
